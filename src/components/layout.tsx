944ad58051b266941ce1d008ad52d8ee5831dfe7
danangekal <danangekal@gmail.com>
2021-02-23 10:46:08 +0700

    First clone project from gatsby-typescript-default-starter

---
+++ src/components/layout.tsx
@@ -0,0 +1,61 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+import { ReactElement, ReactNode } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+
+import Header from './header';
+import './layout.css';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+function Layout({ children }: LayoutProps): ReactElement {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || 'Title'} />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0 1.0875rem 1.45rem',
+        }}
+      >
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: '2rem',
+          }}
+        >
+          ©
+          {' '}
+          {new Date().getFullYear()}
+          , Built with
+          {' '}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
---
+++ src/pages/page-2.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+function SecondPage(): ReactElement {
+  return (
+    <Layout>
+      <SEO title="Page Two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+}
+
+export default SecondPage;
